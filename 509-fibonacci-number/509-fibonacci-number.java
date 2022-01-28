1b1a9503ac80be7cd3c3d9cab99c56adb2859e8c
Mukul Kumar Tiwari <61946154+mukultwr@users.noreply.github.com>
2022-01-28 21:47:16 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (6.84%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        if(n<1){
+            return 0;
+        }
+       int []A=new int [n+1];
+        
+        A[0]=0;
+        
+                A[1]=1;
+        for(int i=2;i<n+1;i++){
+            A[i]=A[i-1]+A[i-2];
+            
+        }
+        
+             return A[n];  
+        
+    }
+}
