0227b1630422a7819c1b110e3a008e81fb517c46
Mukul Kumar Tiwari <61946154+mukultwr@users.noreply.github.com>
2022-01-28 11:49:12 +0530

    Time: 1 ms (96.51%), Space: 50.4 MB (50.34%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -2,15 +2,16 @@ class Solution {
     public void reverseString(char[] s) {
         int start=0;
         int end=s.length-1;
-        function(start,end,s);
+        function(start,s);
         }
      
-    static void function(int start,int end,char []s){
-        if(start>end){
+    static void function(int start ,char []s){
+        int n=s.length;
+        if(start>=n/2){
             return;
         }
-        swap(start,end,s);
-        function(start+1,end-1,s);
+        swap(start,s.length-start-1,s);
+        function(start+1,s);
     }
     
     static void swap(int start,int end,char[]s){
