02c4f584f76650ed3454ecb6adfb850aeeef0a68
Mukul Kumar Tiwari <61946154+mukultwr@users.noreply.github.com>
2022-04-23 10:30:38 +0530

    Time: 1 ms (86.64%), Space: 47.9 MB (37.27%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,22 +1,39 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicates(int[] arr) {
          // HashSet<Integer>set=new HashSet<>();
         // for(int i=0;i<nums.length;i++){
         //     set.add(nums[i]);
         // }
         // return set.size();
-        if(nums.length==0){
-            return 0;
-        }
-        int i=0;
-        for(int j=1;j<nums.length;j++){
-            if(nums[j]!=nums[i]){
-                i++;
-                nums[i]=nums[j];
+        
+        //approach 2
+//         if(nums.length==0){
+//             return 0;
+//         }
+//         int i=0;
+//         for(int j=1;j<nums.length;j++){
+//             if(nums[j]!=nums[i]){
+//                 i++;
+//                 nums[i]=nums[j];
                 
-            }
+//             }
             
+//         }
+//         return i+1;
+//     }
+        int n=arr.length;
+        int []temp=new int [arr.length];
+        temp[0]=arr[0];
+        int res=1;
+        for(int i=1;i<n;i++){
+            if(temp[res-1]!=arr[i]){
+                temp[res]=arr[i];
+                res++;
+            }
         }
-        return i+1;
-    }
+        for(int i=0;i<n;i++){
+            arr[i]=temp[i];
+        }
+        return res;
+}
 }
