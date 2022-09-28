2539fd534d8e1be6b66f0d7ffac2379b396a840c
Mukul Kumar Tiwari <61946154+mukultwr@users.noreply.github.com>
2022-09-28 20:06:19 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (96.41%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,6 +1,22 @@
 class Solution {
     public int climbStairs(int n) {
-        // if(n==0){
+        
+        
+        int []dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    
+    
+}
+
+//recursion
+// if(n==0){
         //     return 1;
         // }
         // if(n==1){
@@ -9,21 +25,33 @@ class Solution {
         // int left=climbStairs(n-1);
         // int right=climbStairs(n-2);
         // return left + right;
-        int []dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        int x=solve(n,dp);
-        return x;
-    }
-    
-    public static int solve(int n,int[]dp){
-        if(n==0){
-            return 1;
-        }
-        if(n==1){
-            return 1;
-        }
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+
+
+//memoization
+// class Solution {
+//     public int climbStairs(int n) {
         
-    }
-}
+        
+//         int []dp=new int[n+1];
+//         Arrays.fill(dp,-1);
+//         int x=solve(n,dp);
+//         return x;
+//     }
+    
+//     public static int solve(int n,int[]dp){
+//         if(n==0){
+//             return 1;
+//         }
+//         if(n==1){
+//             return 1;
+//         }
+//         if(dp[n]!=-1)return dp[n];
+//         return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        
+//     }
+// }
+
+
+//tabulation
+
+
