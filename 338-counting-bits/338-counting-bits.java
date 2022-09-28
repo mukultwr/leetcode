77e96cd2672aa619dcd854582efe95cc6a8472ee
Mukul Kumar Tiwari <61946154+mukultwr@users.noreply.github.com>
2022-09-28 23:08:01 +0530

    Time: 3 ms (64.06%), Space: 48.2 MB (39.10%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -2,7 +2,7 @@ class Solution{
 public int[] countBits(int num) {
     int result[] = new int[num + 1];
     int offset = 1;
-    for (int index = 1; index < num + 1; ++index){
+    for (int index = 1; index < num + 1; index++){
         if (offset * 2 == index){
             offset *= 2;
         }
